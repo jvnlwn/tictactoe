a41297cfe519ad37c59b9f397a5ef90adab57be7
Joe VanLeeuwen <jvnlwn@gmail.com>
2014-01-23 17:34:52 -0500

    got some x's

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    chooseSquare();
+});
+
+function chooseSquare() {
+	$('.square').click(function() {	
+		
+		var squareTaken = $(this).children().length > 0;
+
+		if (!squareTaken) {
+			var template = _.template($('#x').text());
+			$(this).append(template())	
+		}
+	})
+}
